215834504c7131500a0791aee0d47c3cbef6564e
Rostislav Dugin <rostislav.dugin@outlook.com>
2025-12-22 23:07:31 +0300

    optimize page

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,15 +12,34 @@ export default function RootLayout({
         {/* DNS Prefetch & Preconnect for Analytics */}
         <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
         <link rel="dns-prefetch" href="https://www.google-analytics.com" />
+
+        {/* Preload critical fonts */}
+        <link
+          rel="preload"
+          href="/font/Jost-Regular.ttf"
+          as="font"
+          type="font/ttf"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/font/Jost-Bold.ttf"
+          as="font"
+          type="font/ttf"
+          crossOrigin="anonymous"
+        />
+
+        {/* Preload logo */}
+        <link rel="preload" href="/logo.svg" as="image" type="image/svg+xml" />
       </head>
 
       <body style={{ fontFamily: "Jost, sans-serif" }}>
         {children}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-GE01THYR9X"
-          strategy="afterInteractive"
+          strategy="lazyOnload"
         />
-        <Script id="google-analytics" strategy="afterInteractive">
+        <Script id="google-analytics" strategy="lazyOnload">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
