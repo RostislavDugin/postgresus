54da6369236798c65d2408c7c913079d356f6948
Rostislav Dugin <rostislav.dugin@outlook.com>
2025-12-22 22:22:30 +0300

    FIX (analytics): Get rid of events tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,26 +26,6 @@ export default function RootLayout({
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-GE01THYR9X');
-
-            // Goal 1: Track 30 second engagement
-            setTimeout(function() {
-              gtag('event', 'engaged_30_seconds', {
-                'event_category': 'engagement',
-                'event_label': 'time_on_page'
-              });
-            }, 30000);
-
-            // Goal 2: Track GitHub link clicks
-            document.addEventListener('click', function(e) {
-              var link = e.target.closest('a');
-              if (link && link.href && link.href.includes('github.com')) {
-                gtag('event', 'click_github', {
-                  'event_category': 'outbound',
-                  'event_label': link.href,
-                  'transport_type': 'beacon'
-                });
-              }
-            });
           `}
         </Script>
       </body>
