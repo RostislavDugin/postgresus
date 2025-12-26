2a8f23f5b00f6a45d32f83012b1c52edee44b142
Rostislav Dugin <rostislav.dugin@outlook.com>
2025-12-26 15:14:59 +0300

    FEATURE (analytic): Change GA to Rybbit

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         {/* DNS Prefetch & Preconnect for Analytics */}
-        <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
-        <link rel="dns-prefetch" href="https://www.google-analytics.com" />
+        <link rel="dns-prefetch" href="https://rybbit.databasus.com" />
 
         {/* Preload critical fonts */}
         <link
@@ -36,17 +35,10 @@ export default function RootLayout({
       <body style={{ fontFamily: "Jost, sans-serif" }}>
         {children}
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-GE01THYR9X"
+          src="https://rybbit.databasus.com/api/script.js"
+          data-site-id="dc1a72d4fa0d"
           strategy="lazyOnload"
         />
-        <Script id="google-analytics" strategy="lazyOnload">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-GE01THYR9X');
-          `}
-        </Script>
       </body>
     </html>
   );
